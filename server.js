c41b9082c5199f4d6e061db4812ccebfe57a4e11
hpmartins <hpmartins@gmail.com>
2023-11-26 16:16:33 -0800

    start server

---
+++ server.js
@@ -0,0 +1,13 @@
+import { handler } from './build/handler.js';
+import express from 'express';
+import 'dotenv/config';
+
+const app = express();
+
+const port = process.env.PORT ?? 8000;
+
+app.use(handler);
+
+app.listen(port, () => {
+	console.log(`[${new Date().toLocaleTimeString()}] [musica] listening on port ${port}`);
+});
