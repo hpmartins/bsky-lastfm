64315b7ec1e9f25fa1647c699297b73fe499bfbb
hpmartins <hpmartins@gmail.com>
2023-11-29 13:39:10 -0800

    added title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
     import '../app.css';
 </script>
 
+<svelte:head>
+    <title>Musica @ Bluesky</title>
+</svelte:head>
+
 <div class="mx-auto max-w-7xl py-4 sm:pt-14 sm:px-6 lg:px-8">
     <div class="container bg-gray-400 px-3 sm:px-16 py-4 sm:pt-24 text-center shadow-md sm:rounded-3xl">
         <div class="mx-auto max-w-7xl">
